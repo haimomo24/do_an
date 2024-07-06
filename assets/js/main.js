831e29f8876a5dba6794fc693db5c06007a7513e
haimomo24 <135099315+haimomo24@users.noreply.github.com>
2024-07-06 23:05:55 +0700

    push

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,46 +25,88 @@ $(document).ready(function(){
   });
 
 
-//   add sản phẩm  :
+
+
 const API_URL = "https://r3x9h4-3000.csb.app/products";
-const getApi = async(url) =>{
-    let response = await axios.get(url);
-    // console.log(response.data)
-    showProduct(response.data);
-}
-getApi(API_URL);
-let rowJs =document.querySelector(".row-js");
-const showProduct = (data) =>{
-    let HTML = ``;
-    data.forEach(value => {
-        console.log(value);
-        HTML += ` <div class="col-12 col-sm-6 col-md-3">
-                        <div class="cart-wrap">
-                        <a href="./detail.html?id=${value.id}">
-                            <div class="cart-wrap-content">
-                                <img src="${value.image}" alt="">
-                                <h4>${value.name} </h4>
-                                <p>${value.price}</p>
-                                <div class="buy-cart">
-                                    <button>Mua ngay</button>
-                                </div>
-                                
+        let thisPage = 1;
+        const limit = 8;
+        const rowJs = document.querySelector(".row-js");
+        const listPage = document.querySelector('.listPage');
+
+        const getApi = async (url) => {
+            let response = await axios.get(url);
+            showProduct(response.data);
+            loadItem();
+        }
+
+        const showProduct = (data) => {
+            let HTML = '';
+            data.forEach(value => {
+                HTML += `<div class="col-12 col-sm-6 col-md-3 item">
+                            <div class="cart-wrap">
+                                <a href="./detail.html?id=${value.id}">
+                                    <div class="cart-wrap-content">
+                                        <img src="${value.image}" alt="">
+                                        <h4>${value.name}</h4>
+                                        <p>${value.price}</p>
+                                        <div class="buy-cart">
+                                            <button>Mua ngay</button>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
-                            </a>
-                        </div>
-                    </div>`
-    });
+                         </div>`;
+            });
+            rowJs.innerHTML = HTML;
+            loadItem();
+        };
 
-    rowJs.innerHTML = HTML;
-};
+        const loadItem = () => {
+            const items = document.querySelectorAll('.item');
+            let beginGet = limit * (thisPage - 1);
+            let endGet = limit * thisPage - 1;
+            items.forEach((item, key) => {
+                if (key >= beginGet && key <= endGet) {
+                    item.style.display = 'block';
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+            listPagination(items.length);
+        };
 
-// tim kiem 
+        const listPagination = (totalItems) => {
+            const count = Math.ceil(totalItems / limit);
+            listPage.innerHTML = '';
 
+            if (thisPage !== 1) {
+                let prev = document.createElement('li');
+                prev.innerText = 'PREV';
+                prev.onclick = () => changePage(thisPage - 1);
+                listPage.appendChild(prev);
+            }
 
+            for (let i = 1; i <= count; i++) {
+                let newPage = document.createElement('li');
+                newPage.innerText = i;
+                if (i === thisPage) {
+                    newPage.classList.add('active');
+                }
+                newPage.onclick = () => changePage(i);
+                listPage.appendChild(newPage);
+            }
 
-//phân trang :
+            if (thisPage !== count) {
+                let next = document.createElement('li');
+                next.innerText = 'NEXT';
+                next.onclick = () => changePage(thisPage + 1);
+                listPage.appendChild(next);
+            }
+        };
 
+        const changePage = (i) => {
+            thisPage = i;
+            loadItem();
+        };
 
-
-
-
+        getApi(API_URL);
